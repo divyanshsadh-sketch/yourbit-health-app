1cd5615d0763c1c0f386e6c482662e0de8d8ed51
divyanshsadh-sketch <divyansh.sadh@gmail.com>
2026-08-02 13:53:04 +0530

    Deploy YOURBIT Health App update: sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,17 @@
-const CACHE_NAME = 'yourbit-health-v4';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './logo.png'
-];
-
+// Service Worker Cache Buster & Automatic Cache Purge
 self.addEventListener('install', (e) => {
   self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+      return Promise.all(keys.map((key) => caches.delete(key)));
     }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  // Always fetch live fresh copy from server
+  e.respondWith(fetch(e.request));
 });
