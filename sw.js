5e9d7a891856dfbd1bd46b70978a1259f5b6ab15
divyanshsadh-sketch <divyansh.sadh@gmail.com>
2026-08-02 13:32:00 +0530

    Deploy YOURBIT Health App update: sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,36 @@
-const CACHE_NAME = 'yourbit-health-v1';
+const CACHE_NAME = 'yourbit-health-v2';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
-  './manifest.json'
+  './manifest.json',
+  './logo.png'
 ];
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
